252fdd3fdd5201e7694bb2fe9646f7c7a794186c
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-12-09 21:35:54 +1000

    Desktop launcher enabled

--- desktop/src/com/quiptiq/ludumdare/desktop/DesktopLauncher.java
+++ desktop/src/com/quiptiq/ludumdare/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package com.quiptiq.ludumdare.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.quiptiq.ludumdare.MyGdxGame;
+import com.quiptiq.ludumdare.ld35.GraphicsHandler;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		config.width = 1024;
+		config.height = 768;
+		new LwjglApplication(GraphicsHandler.Companion.getGraphicsHandler(), config);
 	}
 }
